423224ed4d3b65c8896bbee67f54318576e88a18
msanzpascua <msanzpascua@aeport1067.acciona.int>
2015-10-05 22:17:51 +0200

    AllIwvgJunittest

--- src/test/java/es/upm/miw/iwvg/junit/AllIwvgJunittests.java
+++ src/test/java/es/upm/miw/iwvg/junit/AllIwvgJunittests.java
@@ -1,5 +1,9 @@
 package es.upm.miw.iwvg.junit;
 
+//import static org.junit.Assert.assertEquals;
+
+//import org.junit.Before;
+//import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -7,10 +11,13 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
     PointTest.class, 
-    //DecimalCollectionTest.class,
-    //FractionTest.class,
-    //UserTest.class
+    DecimalCollectionTest.class,
+    FractionTest.class,
+    UserTest.class
 })
 public class AllIwvgJunittests {
-
+    
+    
+    
+    
 }
